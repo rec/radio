a703df899f2f941ff34d142ecc35f70decab76c4
Tom Swirly <tom@swirly.com>
2012-01-26 22:07:44 -0500

    nice new ensos

--- public_html/js/map.js
+++ public_html/js/map.js
@@ -7,11 +7,25 @@ function GoogleMap() {
                  mapTypeId: google.maps.MapTypeId.ROADMAP };
   var canvas = document.getElementById("map_canvas");
   var map = new google.maps.Map(canvas, config);
+
+  var ensoo = new google.maps.MarkerImage(
+      'images/ensoo.gif',
+      new google.maps.Size(32, 32),
+      new google.maps.Point(0,0),
+      new google.maps.Point(16, 16));
+
+  var ensoo_shadow = new google.maps.MarkerImage(
+      'images/ensoo_shadow.gif',
+      new google.maps.Size(32, 32),
+      new google.maps.Point(0,0),
+      new google.maps.Point(16, 16));
+
   var marker = new google.maps.Marker({
       position: swirlyRadio,
       animation: google.maps.Animation.DROP,
       map: map,
-      icon: 'images/favico.gif',
+      icon: 'images/ensoo-tiny.gif',
+      shadow: 'images/ensoo-shadow-tiny.gif',
       title:'swirly radio international'});
 };
 
